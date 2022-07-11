b16ea65f76a390ef716914fe2d9b460e76bcebef
LetsMelon <domi.m@outlook.com>
2022-07-11 18:31:46 +0200

    remove use's in trait description

--- rusvid_lib/src/renderer/mod.rs
+++ rusvid_lib/src/renderer/mod.rs
@@ -1,18 +1,13 @@
-use anyhow::Result;
-use std::path::PathBuf;
-use std::rc::Rc;
-use usvg::PathData;
-
 use crate::composition::Composition;
 
 pub mod ffmpeg;
 
 pub trait Renderer {
-    fn render<P: Into<PathBuf>>(
+    fn render<P: Into<std::path::PathBuf>>(
         &self,
         composition: Composition,
         out_path: P,
         tmp_path: P,
-        position: Rc<PathData>, // TODO remove this and add a `animation` trait/struct/... in Composition
-    ) -> Result<()>;
+        position: std::rc::Rc<usvg::PathData>, // TODO remove this and add a `animation` trait/struct/... in Composition
+    ) -> anyhow::Result<()>;
 }
