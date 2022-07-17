2b8d5de74c9c6c16c38a7d40ac280b0432cf9c14
LetsMelon <domi.m@outlook.com>
2022-07-17 12:21:34 +0200

    fix docs-tests

--- rusvid_lib/src/animation/curves/mod.rs
+++ rusvid_lib/src/animation/curves/mod.rs
@@ -7,14 +7,13 @@ pub mod linear;
 /// ```rust
 /// use rusvid_lib::animation::curves::linear::Linear;
 /// use rusvid_lib::animation::curves::Function;
+/// use rusvid_lib::animation::curves::Points::Point1d;
 ///
-/// let linear: Linear<f64> = Linear::new(30, 90, 100.0, 300.0).unwrap();
+/// let linear = Linear::new(30, 90, Point1d(100.0), Point1d(300.0)).unwrap();
 ///
-/// assert_eq!(linear.calc(30), 100.0);
-/// assert_eq!(linear.calc(90), 300.0);
-///
-/// assert_eq!(linear.calc(20), 100.0);
-/// assert_eq!(linear.calc(100), 300.0);
+/// assert_eq!(linear.calc(30), Point1d(100.0));
+/// assert_eq!(linear.calc(60), Point1d(200.0));
+/// assert_eq!(linear.calc(90), Point1d(300.0));
 /// ```
 pub trait Function {
     fn new(start_frame: usize, end_frame: usize, start: Points, end: Points) -> Result<Self>
