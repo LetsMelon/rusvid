6ec485b8f523c5a749f3469c25146152cdb58f77
LetsMelon <domi.m@outlook.com>
2022-10-26 00:10:49 +0200

    implement example: comp_effect

---
+++ examples/comp_effect/src/main.rs
@@ -0,0 +1,50 @@
+use std::rc::Rc;
+
+use rusvid_lib::animation::prelude::*;
+use rusvid_lib::figures::prelude::*;
+use rusvid_lib::prelude::*;
+use rusvid_lib::usvg::{Fill, NodeKind, Paint, Path};
+use rusvid_lib::utils::color_from_hex;
+
+fn main() {
+    let resolution = Resolution::HD;
+
+    let mut composition = Composition::builder()
+        .resolution(resolution)
+        .framerate(30)
+        .duration(2)
+        .add_effect(PixelateEffect::new(15, 15))
+        .build();
+    let frames = composition.frames();
+
+    let layer = composition.create_layer().unwrap();
+
+    let circle_size = Point::new(250.0, 250.0);
+    let circle_pos = Point::ZERO;
+    layer
+        .add_to_root(NodeKind::Path(Path {
+            id: "circle".to_string(),
+            fill: Some(Fill {
+                paint: Paint::Color(color_from_hex("#1212FF".to_string()).unwrap()),
+                ..Fill::default()
+            }),
+            data: Rc::new(circle(circle_pos.x, circle_pos.y, circle_size.x)),
+            ..Path::default()
+        }))
+        .unwrap();
+
+    layer.add_animation(PositionAnimation::new(
+        "circle",
+        Cubic::new_with_ease_type(
+            0,
+            frames,
+            circle_pos,
+            resolution.as_point() - (circle_size * 0.5),
+            EaseType::InOut,
+        )
+        .unwrap(),
+    ));
+
+    let mut renderer = FfmpegRenderer::new("comp_effect.mp4", "./out", FrameImageFormat::Png);
+    renderer.render(composition).unwrap()
+}
