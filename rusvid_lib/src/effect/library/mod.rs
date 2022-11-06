1c06a2f03526b61d9968ea295d9eb09321e191d9
LetsMelon <domi.m@outlook.com>
2022-11-06 21:38:51 +0100

    switch to plane: box blur

--- rusvid_lib/src/effect/library/mod.rs
+++ rusvid_lib/src/effect/library/mod.rs
@@ -1,10 +1,10 @@
-// mod box_blur;
+mod box_blur;
 mod color_palette;
 mod gaussian_blur;
 mod grayscale;
 mod pixelate;
 
-// pub use box_blur::BoxBlur;
+pub use box_blur::BoxBlur;
 pub use color_palette::ColorPaletteEffect;
 pub use gaussian_blur::GaussianBlur;
 pub use grayscale::GrayscaleEffect;
