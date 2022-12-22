86dba65d2d4a1faa10b818529e3d0d302fdcd820
LetsMelon <domi.m@outlook.com>
2022-12-22 07:06:51 +0100

    add all transformations

--- crates/wasm/index.js
+++ crates/wasm/index.js
@@ -3,11 +3,21 @@ import init, {
   set_height,
   set_width,
   add_svg,
+  transform_color,
+  transform_stroke,
+  transform_move,
+  transform_position,
 } from "./pkg/rusvid_wasm.js";
 
+function delay(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
 init().then(() => {
-  const WIDTH = 300;
-  const HEIGHT = 300;
+  const WIDTH = 500;
+  const HEIGHT = 500;
 
   set_width(WIDTH);
   set_height(HEIGHT);
@@ -16,35 +26,83 @@ init().then(() => {
   app.width = WIDTH;
   app.height = HEIGHT;
   let ctx = app.getContext("2d");
+  ctx.imageSmoothingEnabled = false;
 
-  function call_render() {
-    console.log("called");
-
-    const c_r = document.getElementById("c_r").value;
-    const c_g = document.getElementById("c_g").value;
-    const c_b = document.getElementById("c_b").value;
-
-    console.log({c_r, c_g, c_b});
-
+  function init_svg() {
     // 1   x y -> Move
     // 2   x y -> Line
     // 255     -> Close
     add_svg(
       new Uint32Array([0, 100, 100, 1, 150, 100, 1, 120, 150, 255]),
-      new Uint8ClampedArray([c_r, c_g, c_b, 255])
+      new Uint8ClampedArray([255, 255, 255, 255])
     );
 
+    transform_stroke(new Uint8ClampedArray([255, 0, 0, 255]), 2.0);
+  }
+
+  function re_render() {
     let pixels = render();
 
     const image = new ImageData(pixels, WIDTH, HEIGHT);
     ctx.putImageData(image, 0, 0);
   }
 
-  // init_triangle();
-  call_render();
+  function transform() {
+    console.log("called");
 
-  document.getElementById("b_render").addEventListener("click", call_render);
-  document.getElementById("c_r").addEventListener('change', call_render);
-  document.getElementById("c_g").addEventListener('change', call_render);
-  document.getElementById("c_b").addEventListener('change', call_render);
+    const c_r = document.getElementById("c_r").value;
+    const c_g = document.getElementById("c_g").value;
+    const c_b = document.getElementById("c_b").value;
+
+    console.log({ red: c_r, green: c_g, blue: c_b });
+
+    transform_color(new Uint8ClampedArray([c_r, c_g, c_b, 255]));
+
+    re_render();
+  }
+
+  init_svg();
+  transform();
+
+  async function animation() {
+    let x = 0;
+    let op_x = 1;
+    let y = 0;
+    let op_y = 1;
+
+    for (let r = 0; r < 255; r += 10) {
+      for (let g = 0; g < 255; g += 10) {
+        for (let b = 0; b < 255; b += 10) {
+          document.getElementById("c_r").value = r;
+          document.getElementById("c_g").value = g;
+          document.getElementById("c_b").value = b;
+
+          transform_position(x, y);
+
+          y += op_y * 1.25;
+          if (y > 450) {
+            op_y = -1;
+          } else if (y < 0) {
+            op_y = 1;
+          }
+
+          x += op_x * 1.75;
+          if (x > 450) {
+            op_x = -1;
+          } else if (x < 0) {
+            op_x = 1;
+          }
+
+          transform();
+          await delay(2);
+        }
+      }
+    }
+  }
+
+  document.getElementById("b_render").addEventListener("click", re_render);
+  document.getElementById("b_animation").addEventListener("click", animation);
+  document.getElementById("c_r").addEventListener("input", transform);
+  document.getElementById("c_g").addEventListener("input", transform);
+  document.getElementById("c_b").addEventListener("input", transform);
 });
--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -3,14 +3,16 @@ use std::sync::Mutex;
 
 use lazy_static::{__Deref, lazy_static};
 use rusvid_core::holder::likes::*;
-use rusvid_core::holder::object::Object;
+use rusvid_core::holder::object::{Object, TransformLogic};
+use rusvid_core::holder::stroke::Stroke;
 use rusvid_core::holder::svg_holder::{SvgHolder, SvgItem};
+use rusvid_core::holder::transform::Transform;
 use rusvid_core::point::Point;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 lazy_static! {
-    static ref HOLDER: Mutex<SvgHolder> = Mutex::new(SvgHolder::new());
+    static ref OBJECT: Mutex<Object> = Mutex::new(Object::new(TypesLike::Svg(SvgHolder::new())));
     static ref WIDTH: Mutex<u32> = Mutex::new(300);
     static ref HEIGHT: Mutex<u32> = Mutex::new(300);
 }
@@ -44,15 +46,78 @@ pub fn render() -> js_sys::Uint8ClampedArray {
     let width = *WIDTH.lock().unwrap().deref();
     let height = *HEIGHT.lock().unwrap().deref();
 
-    let holder = HOLDER.lock().unwrap();
-    let object = Object::new(TypesLike::Svg(holder.deref().clone()));
+    let object = OBJECT.lock().unwrap();
 
     let plane = object.render(width, height).unwrap();
     let data = plane.as_data();
 
-    let my_data = data.iter().flatten().map(|x| *x).collect::<Vec<u8>>();
+    js_sys::Uint8ClampedArray::from(&data.iter().flatten().map(|x| *x).collect::<Vec<u8>>()[..])
+}
 
-    js_sys::Uint8ClampedArray::from(&my_data[..])
+#[wasm_bindgen]
+pub fn transform_color(color: js_sys::Uint8ClampedArray) {
+    let color_vec = color.to_vec();
+
+    let mut object = OBJECT.lock().unwrap();
+    if color_vec.len() == 4 || color_vec.len() == 3 {
+        object
+            .transform(&Transform::Color(Some(ColorLike::Color([
+                color_vec[0],
+                color_vec[1],
+                color_vec[2],
+                255,
+            ]))))
+            .unwrap()
+    } else {
+        object.transform(&Transform::Color(None)).unwrap()
+    }
+}
+
+#[wasm_bindgen]
+pub fn transform_stroke(paint: Option<js_sys::Uint8ClampedArray>, width: Option<f64>) {
+    let paint_vec_option = paint.map(|item| item.to_vec()).unwrap_or_default();
+    let stroke = match (paint_vec_option.len(), width) {
+        (3 | 4, Some(w)) => {
+            let c = ColorLike::Color([
+                paint_vec_option[0],
+                paint_vec_option[1],
+                paint_vec_option[2],
+                255,
+            ]);
+
+            Some(Stroke {
+                paint: c,
+                width: w,
+                ..Default::default()
+            })
+        }
+        _ => None,
+    };
+
+    let mut object = OBJECT.lock().unwrap();
+    object.transform(&Transform::Stroke(stroke)).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn transform_move(x: f64, y: f64) {
+    let mut object = OBJECT.lock().unwrap();
+    object
+        .transform(&Transform::Move(Point::new(x, y)))
+        .unwrap();
+}
+
+#[wasm_bindgen]
+pub fn transform_position(x: f64, y: f64) {
+    let mut object = OBJECT.lock().unwrap();
+    object
+        .transform(&Transform::Position(Point::new(x, y)))
+        .unwrap();
+}
+
+#[wasm_bindgen]
+pub fn transform_visibility(value: bool) {
+    let mut object = OBJECT.lock().unwrap();
+    object.transform(&Transform::Visibility(value)).unwrap();
 }
 
 #[wasm_bindgen]
@@ -106,7 +171,9 @@ pub fn add_svg(data: js_sys::Uint32Array, color: js_sys::Uint8ClampedArray) {
 
     console::log_1(&format!("{:?}", item).into());
 
-    let mut binding = HOLDER.lock();
-    let holder = binding.as_mut().unwrap();
-    holder.add_item(item);
+    let mut binding = OBJECT.lock().unwrap();
+    let types_like = binding.data_mut();
+    if let TypesLike::Svg(svg_holder) = types_like {
+        svg_holder.add_item(item);
+    }
 }
