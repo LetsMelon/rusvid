7dfad62bd27d978cb9d6ae2b0a3ba120c2ca5aee
LetsMelon <domi.m@outlook.com>
2023-02-21 21:19:54 +0100

    remove selfmade AbsDiffEq-function and add some tests

--- crates/core/src/point.rs
+++ crates/core/src/point.rs
@@ -42,19 +42,6 @@ impl Point {
     pub fn y_mut(&mut self) -> &mut f64 {
         &mut self.0.y
     }
-
-    /// Returns true if the absolute difference of all elements between `self` and `rhs` is
-    /// less than or equal to `max_abs_diff`.
-    ///
-    /// This can be used to compare if two vectors contain similar elements. It works best when
-    /// comparing with a known value. The `max_abs_diff` that should be used used depends on
-    /// the values being compared against.
-    ///
-    /// For more see
-    /// [comparing floating point numbers](https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/).
-    pub fn abs_diff_eq(self, rhs: Self, max_abs_diff: f64) -> bool {
-        self.0.abs_diff_eq(rhs.0, max_abs_diff)
-    }
 }
 
 impl From<(f64, f64)> for Point {
@@ -71,18 +58,23 @@ impl AsRef<[f64; 2]> for Point {
 
 impl AbsDiffEq for Point {
     type Epsilon = <f64 as AbsDiffEq>::Epsilon;
+
+    #[cfg_attr(coverage_nightly, no_coverage)]
     fn default_epsilon() -> Self::Epsilon {
         f64::default_epsilon()
     }
+
     fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
         self.as_ref().abs_diff_eq(other.as_ref(), epsilon)
     }
 }
 
 impl RelativeEq for Point {
+    #[cfg_attr(coverage_nightly, no_coverage)]
     fn default_max_relative() -> Self::Epsilon {
         f64::default_max_relative()
     }
+
     fn relative_eq(
         &self,
         other: &Self,
@@ -95,9 +87,11 @@ impl RelativeEq for Point {
 }
 
 impl UlpsEq for Point {
+    #[cfg_attr(coverage_nightly, no_coverage)]
     fn default_max_ulps() -> u32 {
         f64::default_max_ulps()
     }
+
     fn ulps_eq(&self, other: &Self, epsilon: Self::Epsilon, max_ulps: u32) -> bool {
         self.as_ref().ulps_eq(other.as_ref(), epsilon, max_ulps)
     }
@@ -195,4 +189,130 @@ pub trait AsPoint {
     /// assert_eq!(res.as_point() * Point::NEG_ONE, Point::new(-100.0, -100.0));
     /// ```
     fn as_point(&self) -> Point;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Point;
+
+    #[test]
+    fn coordinate_mut() {
+        let mut p = Point::new(25.0, 50.0);
+
+        assert_eq!(p.x(), 25.0);
+        *p.x_mut() = 75.0;
+        assert_eq!(p.x(), 75.0);
+
+        assert_eq!(p.y(), 50.0);
+        *p.y_mut() = 25.0;
+        assert_eq!(p.y(), 25.0);
+    }
+
+    #[test]
+    fn from_tuple() {
+        let tuple = (50.0, -10.0);
+        let p = Point::from(tuple);
+
+        assert_eq!(tuple.0, p.x());
+        assert_eq!(tuple.1, p.y());
+    }
+
+    #[test]
+    fn as_ref() {
+        let p = Point::new(250.0, -300.0);
+        let slice = p.as_ref();
+
+        assert_eq!(p.x(), slice[0]);
+        assert_eq!(p.y(), slice[1]);
+    }
+
+    mod approx {
+        use approx::*;
+
+        use crate::point::Point;
+
+        #[test]
+        fn abs_diff_eq() {
+            let p1 = Point::new(100.0, 200.0);
+            let p2 = Point::new(100.0001, 199.9999);
+            let p3 = Point::new(75.0, 220.0);
+
+            assert_eq!(p1.abs_diff_eq(&p2, 0.0), false);
+            assert_eq!(p1.abs_diff_eq(&p3, 0.0), false);
+
+            assert_eq!(p1.abs_diff_eq(&p2, 0.001), true);
+            assert_eq!(p1.abs_diff_eq(&p3, 0.001), false);
+
+            assert_eq!(p1.abs_diff_eq(&p2, 30.0), true);
+            assert_eq!(p1.abs_diff_eq(&p3, 30.0), true);
+        }
+
+        #[test]
+        fn relative_eq() {
+            let p1 = Point::new(100.0, 200.0);
+            let p2 = Point::new(100.0001, 199.9999);
+            let p3 = Point::new(75.0, 220.0);
+
+            assert_eq!(p1.relative_eq(&p2, 0.0, 0.0), false);
+            assert_eq!(p1.relative_eq(&p3, 0.0, 0.0), false);
+
+            assert_eq!(p1.relative_eq(&p2, 0.001, 0.0), true);
+            assert_eq!(p1.relative_eq(&p3, 0.001, 0.0), false);
+
+            assert_eq!(p1.relative_eq(&p3, 30.0, 0.0), true);
+            assert_eq!(p1.relative_eq(&p2, 30.0, 0.0), true);
+        }
+    }
+
+    mod math {
+        use crate::point::Point;
+
+        #[test]
+        fn add() {
+            let p1 = Point::new(25.0, 50.0);
+            let p2 = Point::new_symmetric(10.0);
+
+            assert_eq!(10.0 + p1, Point::new(35.0, 60.0));
+            assert_eq!(p1 + 10.0, Point::new(35.0, 60.0));
+            assert_eq!(p1 + p2, Point::new(35.0, 60.0));
+
+            let mut p3 = Point::new(35.0, -10.0);
+            p3 += 10.0;
+            assert_eq!(p3, Point::new(45.0, 0.0));
+            p3 += Point::new(-35.0, 100.0);
+            assert_eq!(p3, Point::new(10.0, 100.0));
+        }
+
+        #[test]
+        fn sub() {
+            let p1 = Point::new(25.0, 50.0);
+            let p2 = Point::new_symmetric(10.0);
+
+            assert_eq!(10.0 - p1, Point::new(-15.0, -40.0));
+            assert_eq!(p1 - 10.0, Point::new(15.0, 40.0));
+            assert_eq!(p1 - p2, Point::new(15.0, 40.0));
+
+            let mut p3 = Point::new(35.0, -10.0);
+            p3 -= 10.0;
+            assert_eq!(p3, Point::new(25.0, -20.0));
+            p3 -= Point::new(-35.0, 100.0);
+            assert_eq!(p3, Point::new(60.0, -120.0));
+        }
+
+        #[test]
+        fn mul() {
+            let p1 = Point::new(25.0, 50.0);
+            let p2 = Point::new_symmetric(10.0);
+
+            assert_eq!(10.0 * p1, Point::new(250.0, 500.0));
+            assert_eq!(p1 * 10.0, Point::new(250.0, 500.0));
+            assert_eq!(p1 * p2, Point::new(250.0, 500.0));
+
+            let mut p3 = Point::new(35.0, -10.0);
+            p3 *= 5.7;
+            assert_eq!(p3, Point::new(199.5, -57.0));
+            p3 *= Point::new(-35.0, 100.0);
+            assert_eq!(p3, Point::new(-6982.5, -5700.0));
+        }
+    }
 }
