ac1e82f340c3e09b3fe0e9b4ca82746c23ae30be
LetsMelon <domi.m@outlook.com>
2023-02-21 21:17:55 +0100

    ignore Default trait in coverage report

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(iter_array_chunks)]
 #![feature(variant_count)]
+#![feature(is_some_and)]
+#![cfg_attr(coverage_nightly, feature(no_coverage))]
 
 pub mod frame_image_format;
 pub mod holder;
